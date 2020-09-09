2656c6113888529fad732917a5f6ecd4f0cbcc31
=Almlett <isc.andradenavarro@gmail.com>
2020-09-09 12:16:19 -0700

    exclude entorno

--- conection/urls.py
+++ conection/urls.py
@@ -1,12 +1,13 @@
 # -*- encoding: utf-8 -*-
 
 from django.conf.urls import patterns, url, include
-from .views import ArticulosViewSet, ClientesViewSet, home
+from .views import ArticulosViewSet, ClientesViewSet, DoctosViewSet, home
 
 from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register(r'articulos', ArticulosViewSet, base_name='articulos_vs')
 router.register(r'clientes', ClientesViewSet, base_name='clientes_vs')
+router.register(r'doctos', DoctosViewSet, base_name='doctos_vs')
 
 
 
